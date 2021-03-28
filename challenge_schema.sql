86786394ea4fd971f17e1eb96a6970d5f78d70d0
yaserkassam <yaser.m.kassam@gmail.com>
2021-03-28 17:38:24 -0400

    week 16 challenge

---
+++ challenge_schema.sql
@@ -0,0 +1,32 @@
+CREATE TABLE review_id_table (
+  review_id TEXT PRIMARY KEY NOT NULL,
+  customer_id INTEGER,
+  product_id TEXT,
+  product_parent INTEGER,
+  review_date DATE -- this should be in the formate yyyy-mm-dd
+);
+SELECT * FROM review_id_table;
+-- This table will contain only unique values
+CREATE TABLE products_table (
+  product_id TEXT PRIMARY KEY NOT NULL,
+  product_title TEXT
+);
+SELECT * FROM products_table;
+
+-- Customer table for first data set
+CREATE TABLE customers_table (
+  customer_id INT PRIMARY KEY NOT NULL UNIQUE,
+  customer_count INT
+);
+SELECT * FROM customers_table;
+-- vine table
+CREATE TABLE vine_table (
+  review_id TEXT PRIMARY KEY,
+  star_rating INTEGER,
+  helpful_votes INTEGER,
+  total_votes INTEGER,
+  vine TEXT,
+  verified_purchase TEXT
+);
+SELECT * FROM vine_table;
+SELECT * FROM products_table;
